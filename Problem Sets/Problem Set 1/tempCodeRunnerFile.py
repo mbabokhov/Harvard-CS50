152b7de5f5d50cc0b5f22455deb9af1eb08f1284
mbabokhov <mbabokhov@gmail.com>
2025-04-02 17:26:26 -0400

    Delete Problem Sets/Problem Set 1/tempCodeRunnerFile.py

--- Problem Sets/Problem Set 1/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-if file_name.endswith('.png'):
-#     print('image/' + file_name)
-# elif file_name.endswith('.pdf', '.txt'):
-#     print('text/' + file_name)
-# elif file_name.endswith('.zip'):
-#     print('application/' + file_name)
-# else:
-#     print('application/octet-stream')
