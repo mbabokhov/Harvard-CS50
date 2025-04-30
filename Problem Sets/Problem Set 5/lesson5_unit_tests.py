a5840268a52d268c03408ce62ec916d34b5aa53b
Michael Babokhov <mbabokhov@gmail.com>
2025-04-30 14:33:36 -0400

    Update lesson5_unit_tests.py

--- Problem Sets/Problem Set 5/lesson5_unit_tests.py
+++ Problem Sets/Problem Set 5/lesson5_unit_tests.py
@@ -1,1 +1,16 @@
 
+# Run unit tests to check for bugs in code
+
+def main():
+    x = input('What is x? ')
+    print('x squared is', square(x))
+
+
+def square(n):
+    return n * n
+
+
+if __name__ == '__main__':
+    main()
+# But code as it stands can't accept letters or floats without crashing
+# Test by making new program lesson5_test_calculator.py
